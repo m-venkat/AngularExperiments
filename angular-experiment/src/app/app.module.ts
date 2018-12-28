bd82c2381008dc884877a21300ae75c036562c7e
Venkatesh Muniyandi <venkatesh_muniyandi@dialog.com.au>
2018-12-29 10:43:54 +1100

    Fix stackblitz issue

--- angular-experiment/src/app/app.module.ts
+++ angular-experiment/src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, Component, } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { BindingsComponent } from './bindings/bindings.component';
 import { DirectivesComponent } from './directives/directives.component';
-import {HomeComponent } from './home/home.component';
+import { HomeComponent } from './home/home.component';
 import { NotreadyComponent } from './notready/notready.component';
 import { PlainworkerComponent } from './plainworker/plainworker.component';
 import { AppRoutingModule } from './/app-routing.module';
-import {FormsModule } from '@angular/forms';
+
 import { LonrunningproblemComponent } from './lonrunningproblem/lonrunningproblem.component';
-
-
 
 @NgModule({
   imports: [
@@ -25,14 +25,8 @@ import { LonrunningproblemComponent } from './lonrunningproblem/lonrunningproble
     HomeComponent,
     NotreadyComponent,
     PlainworkerComponent,
-    LonrunningproblemComponent,
+    LonrunningproblemComponent
   ],
-  exports: [
-    FormsModule
-  ],
-  providers: [],
   bootstrap: [AppComponent],
 })
-
-
 export class AppModule { }
